71bbc256fbb8756cdf2d7926145897f273e8397d
AleksandraBerezikova <55293669+AleksandraBerezikova@users.noreply.github.com>
2019-12-07 23:50:05 +0300

    Create Задача 8.cpp

---
+++ 2019.12.07.ДЗ-3/Задача 8.cpp
@@ -0,0 +1,64 @@
+/*
+ Напишите программу, находящую k-ую порядковую статистику. Программа должна работать за время O(n) в среднем.
+
+ */
+#include <iostream>
+
+using namespace std;
+
+int part(int arr[], int l, int r)
+{
+    int x = arr[r], i = l;
+    for (int j = l; j <= r - 1; j++)
+    {
+        if (arr[j] <= x)
+        {
+            swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+    swap(arr[i], arr[r]);
+    return i;
+}
+
+int kt(int arr[], int l, int r, int k)
+{
+
+    if (k > 0 && k <= r - l + 1) {
+
+
+        int index = part(arr, l, r);
+
+       
+        if (index - l == k - 1)
+            return arr[index];
+
+       
+        if (index - l > k - 1)
+            return kt(arr, l, index - 1, k);
+
+        
+        return kt(arr, index + 1, r,
+                           k - index + l - 1);
+    }
+
+
+    return INT_MAX;
+}
+
+
+int main()
+{
+
+    int n,k;
+    cin >> k >> n;
+    int *v = new int[n];
+    for(int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
+
+    cout << kt (v, 0, n - 1, k);
+    return 0;
+}
+
